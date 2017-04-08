a663666af8ff6664c5364d61e507e200a7f14dd8
Guillermo Peralta Scura <gperaltascura@gmail.com>
2017-04-08 11:44:37 -0400

    Initial Commit

---
+++ ReSwiftUndo/ReSwiftUndo.h
@@ -0,0 +1,19 @@
+//
+//  ReSwiftUndo.h
+//  ReSwiftUndo
+//
+//  Created by Guillermo Peralta Scura on 8/4/17.
+//  Copyright © 2017 voluntadpear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReSwiftUndo.
+FOUNDATION_EXPORT double ReSwiftUndoVersionNumber;
+
+//! Project version string for ReSwiftUndo.
+FOUNDATION_EXPORT const unsigned char ReSwiftUndoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSwiftUndo/PublicHeader.h>
+
+
